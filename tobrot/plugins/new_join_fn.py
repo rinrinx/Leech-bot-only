6787b3d50c00f9712911b8616cba137351fcd1f2
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-07-30 09:43:57 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -19,11 +19,11 @@ from tobrot import (
 )
 
 
-async def new_join_f(client, message):
-    chat_type = message.chat.type
-    if chat_type != "private":
-        await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
-        # leave chat
+#async def new_join_f(client, message):
+    #chat_type = message.chat.type
+    #if chat_type != "private":
+        #await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
+         # leave chat
         await client.leave_chat(
             chat_id=message.chat.id,
             delete=True
